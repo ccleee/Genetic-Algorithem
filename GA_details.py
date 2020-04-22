79f54f7c0b0efe44a00a6e6b965801d5cb92dfdf
cclee0204 <cclee990204@gmail.com>
2020-04-22 23:05:47 +0800

    GA for maxnum in function

--- GA_details.py
+++ GA_details.py
@@ -3,70 +3,54 @@ import math
 import numpy as np
 
 
+# 10-->2
+def encode(num, range0, length):
+    n = '{:0>64b}'.format(int(num*10000) - int(range0*10000))[-length:]
+    #print(list(map(int, n)))
+    return list(map(int, n))
+
+
 # Step1:初始化种群个体
 def species_origin(num, length):
     # 10*33的二维数组
-    population = np.random.randint(0, 2, [num, length])
+    population = np.zeros([num, length])
+    for i in range(num):
+        x1 = np.random.uniform(-3.0, 12.1)
+        x2 = np.random.uniform(4.1, 5.8)
+        # print(x1, x2)
+        population[i][0:18] = encode(x1, -3.0, 18)
+        population[i][18:33] = encode(x2, 4.1, 15)
     # print(population)
     return population
 
 
 # Step2计算个体适应度值
-# 2.1二进制转十进制
-def transition(population, length1, length2):
-    # 10*2的二维数组，存放转化结果
-    decimal = np.zeros([len(population), 2])
-    for i in range(len(population)):
-        # （0~17位）
-        f = 17
-        for j in range(length1):
-            # print(i,j)
-            # 高18位十进制结果
-            decimal[i][0] += population[i][j] * (math.pow(2, f))
-            f -= 1
-        # （18~32位）
-        f = 14
-        for k in range(length1, length1 + length2):
-            # 低15位数十进制结果
-            decimal[i][1] += population[i][k] * (math.pow(2, f))
-            f -= 1
-    return decimal
-
-
-# 2.2 取值范围映射（分别求出随机数对应的x1，x2的取值)
-def decode(population, length1, length2):
+# 2.1 求x1，x2
+def decode(population):
     # 10*2的二维数组，存放最后结果
     domain = np.zeros([len(population), 2])
-    decimal = transition(population, length1, length2)
-    d1 = (12.1 - (-3.0)) / (math.pow(2, length1) - 1)
-    d2 = (5.8 - 4.1) / (math.pow(2, length2) - 1)
     for i in range(len(population)):
-        # 种群个体数值x1的取值
-        domain[i][0] = decimal[i][0] * d1 - 3
-        # 种群个体数值x2的取值
-        domain[i][1] = decimal[i][1] * d2 + 4.8
-    # print(decimal)
+        x1 = int("".join(map(lambda x: str(int(x)), population[i][0:18])), 2)
+        domain[i][0] = (x1 - 3.0 * 10000) / 10000
+        x2 = int("".join(map(lambda x: str(int(x)), population[i][18:33])), 2)
+        domain[i][1] = (x2 + 4.1 * 10000) / 10000
     # print(domain)
     return domain
 
 
-# Step2.2代入适应度函数并去掉负值
+# 2.2求适应度
 def get_fitness(domain):
     fitness = np.zeros(len(domain))
     pi = math.pi
     sin = math.sin
     for i in range(len(domain)):
-        # 将原函数作为适应度函数求值，越大适应度越高
         fitness[i] = 21.5 + domain[i, 0] * sin(4 * pi * domain[i, 0]) + domain[i, 1] * sin(20 * pi * domain[i, 1])
-        if fitness[i] < 0:
-            fitness[i] = 0.0
-    #print(fitness)
+    # print(fitness)
     return fitness
 
 
 # Step3轮盘赌进行自然选择及交叉
-
-# Step3.1计算个体的累积适应度
+# Step3.1计算个体的累计适应度
 def get_cumfitness(fitness):
     # 存放累计适应度结果
     cumfitness = np.zeros(len(fitness))
@@ -74,44 +58,78 @@ def get_cumfitness(fitness):
     temp = 0
     for i in range(len(fitness)):
         temp += fitness[i]
-        # 转化为百分比
         cumfitness[i] = temp / total
-    #print(cumfitness)
+    # print(cumfitness)
     return cumfitness
 
 
-
 # Step3.2轮盘赌选择进入交叉池的个体
-def select(cumfitness, population):
+def first_select(cumfitness,mutated):
+    new = np.zeros(np.shape(mutated))
+    # 交叉变异后种群新的x1x2
+    domain = decode(mutated)
+    # 新的适应度
+    fitness = get_fitness(domain)
+    # 按适应度排序
+    s = sorted(fitness, reverse=True)
+    # 前3个直接进入下一代
+    # 前20%个直接进入下一代
+    n = int(len(mutated) * 0.3)
+    for i in range(n):
+        for j in range(len(mutated)):
+            if fitness[j] == s[i]:
+                new[i] = mutated[j]
+                break
+    # 新种群累计适应度
+    cumfitness = get_cumfitness(fitness)
+    # 进行第二次轮盘赌找后7个
+    new_selected = select(cumfitness,mutated,len(mutated))
+    for i in range(3, len(mutated)):
+        new[i] = new_selected[i]
+    # print(new)
+    return new
+
+
+def select(cumfitness, population, n):
     # 初始化结果数组10*33
-    selected = np.zeros(np.shape(population))
-    for i in range(len(population)):
+    selected = np.zeros([n, np.shape(population)[1]])
+    for i in range(n):
         r = np.random.rand()
         # 根据随机数大小所在的累计适应度区间，选择进入交叉池的个体
         for j in range(len(cumfitness)):
             if r < cumfitness[j]:
-                selected[i] = population[j]
-                break
+                if np.all(population[j] == 0):
+                    i -= 1
+                    break
+                else:
+                    selected[i] = population[j]
+                    break
+    # print(selected)
     return selected
-#step3.2 保留最优个体并进行轮盘赌
+
 
 # Step3.3 选中个体进行交叉
 def crossover(selected, pc):
-    # 进行10/5=2次循环，两两交配
+    # print(selected)
     for i in range(0, len(selected), 2):
         # 产生随机数，判断是否交叉
         r = np.random.rand()
         if r < pc:
             # 生成两个交叉点
-            cp1 = np.random.randint(0, len(selected[0]))
-            cp2 = np.random.randint(0, len(selected[0]))
+            cp1 = np.random.randint(0, len(selected[0])+1)
+            cp2 = np.random.randint(0, len(selected[0])+1)
             # 确保交叉点1<交叉点2
             if cp1 > cp2:
                 cp1, cp2 = cp2, cp1
-            #  开始交叉
+            # print(cp1, cp2)
             for j in range(cp1, cp2):
-                #  相邻两个染色体进行交叉，交换两个交叉点间的所有基因
+                #  相邻两个交换两个交叉点间的所有基因
                 selected[i][j], selected[i + 1][j] = selected[i + 1][j], selected[i][j]
+                x = decode(selected[i:i+2])
+                if (x[0, 0] < -3.0) | (x[0, 0] > 12.1) | (x[0, 1] < 4.1) | (x[0, 1] > 5.8):
+                    selected[i] = np.zeros([1, np.shape(selected)[1]])
+                if (x[1, 0] < -3.0) | (x[1, 0] > 12.1) | (x[1, 1] < 4.1) | (x[1, 1] > 5.8):
+                    selected[i+1] = np.zeros([1, np.shape(selected)[1]])
     return selected
 
 
@@ -123,30 +141,32 @@ def mutate(crossed, pm):
             r = np.random.rand()
             # 若发生变异，0->1,1->0
             if r < pm:
-                crossed[i][j] = (crossed[i][j]+1) % 2
+                crossed[i, j] = (crossed[i, j]+1) % 2
+                x1, x2 = decode(crossed[i:i+2])[0]
+                if (x1 < -3.0) | (x1 > 12.1) | (x2 < 4.1) | (x2 > 5.8):
+                    crossed[i] = np.zeros([1, np.shape(crossed)[1]])
     return crossed
 
 
 # Step5 选取下一代种群
-def new_select(mutated, length1, length2):
+def new_select(mutated):
     new = np.zeros(np.shape(mutated))
-    # 交叉变异后种群新的x1x2
-    domain = decode(mutated, length1, length2)
     # 新的适应度
-    fitness = get_fitness(domain)
+    fitness = get_fitness(decode(mutated))
+    # print(fitness)
     # 按适应度排序
     s = sorted(fitness, reverse=True)
-    # 前2个直接进入下一代
-    for i in range(len(mutated)):
-        if fitness[i] == s[0]:
-            new[0] = mutated[i]
-        elif fitness[i] == s[1]:
-            new[1] = mutated[i]
-    # 新种群累计适应度
+    # 前20%个直接进入下一代
+    n = int(len(mutated)*0.3)
+    for i in range(n):
+        for j in range(len(mutated)):
+            if fitness[j] == s[i]:
+                new[i] = mutated[j]
+                break
+    # 新累计适应度
     cumfitness = get_cumfitness(fitness)
-    # 进行第二次轮盘赌找后8个
-    new_selected = select(cumfitness, mutated)
-    for i in range(2, len(mutated)):
-        new[i] = new_selected[i]
+    # 进行第二次轮盘赌
+    new_selected = select(cumfitness, mutated, len(mutated)-n)
+    new[n:] = new_selected
     # print(new)
     return new
--- GA_main.py
+++ GA_main.py
@@ -1,43 +1,33 @@
-# -*-coding:utf-8 -*-
-import numpy as np
-from GA_details import species_origin, decode, get_fitness, get_cumfitness, select, crossover, mutate, new_select
-
-# Step0:编码方案
+from GA_details import species_origin, decode, get_fitness, get_cumfitness, select, crossover, mutate, new_select,first_select
 # 个体数目
-num = 10
-# 当前进化代数
-t = 0
-# 染色体长度1
-length1 = 18
-# 染色体长度2
-length2 = 15
+num = 100
 # 染色体长度：18+15
-length = length1 + length2
+length = 33
 # 交叉概率
 pc = 0.6
 # 变异概率
 pm = 0.01
 # 总进化代数
-sum1 = 50
-
-population = species_origin(num, length)  # 初始化种群
-
+sum1 = 1000
+for j in range(0,20):
+# 初始化种群
+    population = species_origin(num, length)
 # 进化过程
-for i in range(sum1):
-    # x1,x2
-    domain = decode(population, length1, length2)
-    # 种群适应度
-    fitness = get_fitness(domain)
-    # 种群累积适应度概率
-    cumfitness = get_cumfitness(fitness)
-    # 轮盘赌选出配对个体
-    selected = new_select(cumfitness,mutated, length1, length2)
-    # 两点交叉
-    crossed = crossover(selected, pc)
-    # 变异
-    mutated = mutate(crossed, pm)
-    # 更新下一代
-    population = new_select(cumfitness,mutated, length1, length2)
-domain = decode(population, length1, length2)
-fitness = get_fitness(domain)
-print("最终结果：", fitness[0])
+
+    for i in range(sum1):
+        # 求个体累计适应度
+        cumfitness = get_cumfitness(get_fitness(decode(population)))
+        # 轮盘赌选10个
+        selected = first_select(cumfitness,population)
+        #selected = select(cumfitness, population, len(population))
+        # 两点交叉
+        crossed = crossover(selected, pc)
+        # 变异
+        mutated = mutate(crossed, pm)
+        # 更新下一代
+        population = new_select(mutated)
+        #if i>sum1/2:
+        #   pm = pm/2
+    s = sorted(get_fitness(decode(population)), reverse=True)
+    print("最终结果：", round(s[0],4))
+    # 38.8503
